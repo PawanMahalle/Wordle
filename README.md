767961f181958c9669f654230db9a22d2cf50723
Pawan Mahalle <pawan.g.mahalle@gmail.com>
2022-01-09 19:38:41 -0800

    Add README.md

---
+++ README.md
@@ -0,0 +1,65 @@
+# Wordle 
+
+### What is Wordle?
+Wordle is a daily word game which asks users to guess 
+a 5-letter word and gives hints to reach to the correct
+guess.
+
+Official link: https://www.powerlanguage.co.uk/wordle/
+
+Other: https://octokatherine.github.io/word-master/
+(I know it's too hard to wait 24 hours for next word)
+
+Twitter: https://twitter.com/search?q=%23wordle
+
+### What's this solution?
+This solution is a simple java class to help solve Wordle 
+puzzle online.
+
+The users need to play the first round on Worlde UI where
+they submit the first guess and get a feedback. As a next step,
+users need to submit the guess and feedback shown by wordle UI
+to the program. The program then suggests 10 potential words 
+for next round.
+
+```
+Enter input: INDIA
+Enter feedback (X -> Gray/White, Y -> yellow, G -> green): GYXXX
+Suggestion(s):
+ICING
+ICONS
+IKONS
+IMINE
+IMINO
+IPPON
+IRING
+IRONE
+IRONS
+IRONY
+```
+
+### My guesses using the program:
+
+1. Wordle 204 6/6 (Jan 9, 2022)
+
+   🟨⬜⬜⬜🟨
+
+   ⬜⬜🟩🟩🟩
+
+   ⬜⬜🟩🟩🟩
+
+   ⬜⬜🟩🟩🟩
+
+   ⬜🟩🟩🟩🟩
+
+   🟩🟩🟩🟩🟩
+
+2. Wordle 205 4/6 (Jan 10, 2022)
+
+   ⬜🟨⬜🟩⬜
+
+   ⬜⬜⬜🟩🟨
+
+   🟨⬜⬜🟩🟩
+   
+   🟩🟩🟩🟩🟩
--- src/com/wordle/Main.java
+++ src/com/wordle/Main.java
@@ -29,7 +29,7 @@ public class Main {
 
         /**
          * Maintaining separate regex for each position and
-         * update the regex based on feedback from wordle UI.
+         * update the regex based on feedback from Wordle UI.
          */
         List<String> regexList = initializeRegex();
 
@@ -40,18 +40,18 @@ public class Main {
             System.out.print("Enter input: ");
             String guess = readInput();
 
-            System.out.print("Enter feedback (X -> Gray, Y -> yellow, G -> green): ");
+            System.out.print("Enter feedback (X -> Gray/White, Y -> yellow, G -> green): ");
             String feedback = readInput();
 
             updateRegex(guess, feedback, detectedLetters, regexList);
 
+            System.out.println("Suggestion(s):");
             printTopMatches(detectedLetters, regexList, wordList);
         }
     }
 
     private static void printTopMatches(List<Character> detectedLetters, List<String> regexList, List<String> wordList) {
         String regex = regexList.stream().collect(Collectors.joining());
-
         int printCount = 0;
         for (int wordIndex = 0; wordIndex < wordList.size(); wordIndex++) {
             String currentWord = wordList.get(wordIndex);
@@ -75,7 +75,7 @@ public class Main {
     }
 
     /**
-     * Updates regex based on the feedack.
+     * Updates regex based on the feedback.
      */
     private static void updateRegex(String guess, String feedback, List<Character> detectedLetters, List<String> regexList) {
         for (int pos = 0; pos < WORD_LENGTH; pos++) {
@@ -84,15 +84,16 @@ public class Main {
             char feedbackForCurrentChar = feedback.charAt(pos);
 
             if (isGrayed(feedbackForCurrentChar)) {
-                    /* When the letter is grayed then the letter is not part of the word in general
-                     and hence should be removed from regex for all positions
-                     However, in some cases, if the letter repeated and already marked as yellow or green then also
-                     the letter is grayed and in that case we should not remove the letter from regex for all the positions.
+                /*
+                 When the letter is grayed then the letter is not part of the word in general
+                 and hence should be removed from regex for all positions
+                 However, in some cases, if the letter repeated and already marked as yellow or green then also
+                 the letter is grayed and in that case we should not remove the letter from regex for all the positions.
 
-                     Hence, if the letter is grayed but detected a valid letter then only remove it from regex for
-                     current position.
-                     Otherwise, remove from regex for all position.
-                    */
+                 Hence, if the letter is grayed but detected a valid letter then only remove it from regex for
+                 current position.
+                 Otherwise, remove from regex for all position.
+                 */
                 if (detectedLetters.contains(currentChar)) {
                     String currRegex = regexList.get(pos);
                     String updatedRegex = currRegex.replace(currentChar, '\0');
